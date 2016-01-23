3133f61f835ecafc81da114313094b8eb214e403
benjaminluck <info@benjaminluck.com>
2016-01-23 02:40:17 +0100

    Included background imagery, about pages.

--- slides/slide_about.php
+++ slides/slide_about.php
@@ -1,26 +1,11 @@
 <div id="slide-0" class="slide">
                 <a title="#about"></a>
-                <div id="topRow" class="row">
-                    <div class="col-md-12" style="min-height: 15%">
-                        <div class="col-md-6 col-md-offset-3" style="margin-top:2%;">
-                            <div style="text-align: center">
-                                
+                <div id="content" class="row">
+                        <div class="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3">
+                            <div class="text-size-full margin-top-55pc" style="text-align: center">
+                                DEVORM was founded in Amsterdam in 2014 and focusses on the coolest music from the coolest artists. We are so fresh and so unique. Show me the money. Je gaat mij bellen toch?
+contact: devormrecords@gmail.com
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div id="mid" class="row col-md-12">
-                    <div class="col-md-6 col-md-offset-3 hidden-xs text-center"></div>
-                </div>
-                <div id="bottomRow" class="row col-md-12">
-                    <div class="col-md-6 col-md-offset-3" style="margin-top:20px;">
-                        <div style="text-align: center">
-                            <div class="track" style="text-size: 10px; padding: 5px; border: 3px solid black; width: 13%; margin:auto;"><b>ABOUT</b></div></div>
-                    </div>
-                    <div class="col-md-6 col-md-offset-3" style="margin-top:10px;">
-                        <div style="text-align: center">
-                            
-                        </div>
-                    </div>
                 </div>
             </div>
