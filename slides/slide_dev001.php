f0678b6f8932395b1c270c2b74021b0b9801284e
benjaminluck <info@benjaminluck.com>
2016-01-26 00:46:40 +0100

    Fixed rows and cols

--- slides/slide_dev001.php
+++ slides/slide_dev001.php
@@ -1,6 +1,6 @@
 <div id="dev001" class="slide">
                 <a title="#dev001"></a>
-                <div id="topRow" class="row">
+                <div id="content" class="row">
                     <div class="col-md-12" style="margin-top:20px;">
                         <div style="text-align: center"></div>
                     </div>
@@ -11,13 +11,9 @@
                             </div>
                         </div>
                     </div>
-                </div>
-                <div id="mid" class="row col-md-12">
                     <div class="col-md-6 col-md-offset-3 col-xs-12 text-center">
                         <iframe style="border: 0; width: 450px; height: 450px;" src="https://bandcamp.com/EmbeddedPlayer/album=2804338416/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/" seamless><a href="http://devormrecords.bandcamp.com/album/what-world-when">What World When by Eigenheimer x Individualism</a></iframe>
                     </div>
-                </div>
-                <div id="bottomRow" class="row col-md-12">
                     <div class="col-md-6 col-md-offset-3" style="margin-top:10px;">
                         <div class="track" style="text-align: center">
                             WHAT WORLD WHEN <br>
--- slides/slide_dev002.php
+++ slides/slide_dev002.php
@@ -1,6 +1,6 @@
 <div id="dev002" class="slide">
                 <a title="#dev002"></a>
-                <div id="topRow" class="row">
+                <div id="content" class="row">
                     <div class="col-md-12" style="margin-top:20px;">
                         <div style="text-align: center"></div>
                     </div>
@@ -11,13 +11,9 @@
                             </div>
                         </div>
                     </div>
-                </div>
-                <div id="mid" class="row col-md-12">
                     <div class="col-md-6 col-md-offset-3 col-xs-12 text-center">
                         <iframe style="border: 0; width: 450px; height: 450px;" src="https://bandcamp.com/EmbeddedPlayer/album=2796734636/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/" seamless><a href="http://devormrecords.bandcamp.com/album/mune">MUNE by TYRO</a></iframe>
                     </div>
-                </div>
-                <div id="bottomRow" class="row col-md-12">
                     <div class="col-md-6 col-md-offset-3" style="margin-top:10px;">
                         <div class="track" style="text-align: center">
                             MUNE <br>
