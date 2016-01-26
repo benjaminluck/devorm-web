1c077b418f6da1ddb14d898e114f5c3325a4e615
benjaminluck <info@benjaminluck.com>
2016-01-26 02:03:55 +0100

    Added splash screen screen handling

--- js/page.js
+++ js/page.js
@@ -1,7 +1,14 @@
 function moveToSlickSlide(toSlide){
     lazyCollapse(250);
     $('.container').slick('slickGoTo', toSlide);
-    
+}
+
+function moveThroughSplash(){
+    $('.nav').css('opacity', 1);
+    $('.container').slick('slickGoTo', 1);
+    $('.container').slick('slickRemove', 0);
+    $('.conatiner').slick('slickSetOption','accesibility', true);
+    $('.conatiner').slick('slickSetOption','draggable', true);
 }
 
 function lazyCollapse(time) {
---
+++ slides/slide_splash.php
@@ -0,0 +1,10 @@
+<div id="slide-0" class="slide">
+                <a title="#about"></a>
+                <div id="content" class="row">
+                        <div class="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3">
+                            <div class="text-size-full margin-top-55pc" style="text-align: center">
+                                DEVORM <br> <span id="gate">ENTER</span>
+                            </div>
+                        </div>
+                </div>
+            </div>
