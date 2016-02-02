f4efec90d189435ad38a9f72bf7c1cd373b34090
benjaminluck <info@benjaminluck.com>
2016-02-03 00:05:35 +0100

    unveil

--- js/page.js
+++ js/page.js
@@ -5,14 +5,25 @@ function moveToSlickSlide(toSlide){
 
 function moveThroughSplash(){
     $('.nav').css('opacity', 1);
-    $('.container').slick('slickGoTo', 1);
-    $('.container').slick('slickRemove', 0);
+    unveilSplash(500,"#slide-splash","slide-0",1,0);
+    //$('.container').slick('slickGoTo', 1);
+    //$('.container').slick('slickRemove', 0);
     $('.container').slick('slickSetOption','accessibility', true);
-    $('.conatiner').slick('slickSetOption','draggable', true);
+    $('.container').slick('slickSetOption','draggable', true);
 }
 
 function lazyCollapse(time) {
     setTimeout(function () {
             $('#nav-collapse-btn').click();
     }, time);
+}
+
+function unveilSplash(time, elementA, elementB, slideNoA, slideNoB){
+    $(elementA).css('opacity',0);
+    $(elementB).css('opacity',0);
+    setTimeout(function () {
+            $('.container').slick('slickGoTo', slideNoA);
+            $('.container').slick('slickRemove', slideNoA);
+            $(elementB).css('opacity',1);
+    }, time);  
 }
